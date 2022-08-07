b981805b8676bfc71489b8a528fd2de39a4cbed7
anok70 <anok70@mail.ru>
2022-08-07 16:12:19 +0300

    completed task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrWords = [];
+    let a = [];
+    let b = [];
+    let strResult = [];
+    
+    for (i = 0, j = 0; i < expr.length; i += 10, j++) {
+        arrWords[j] = expr.slice(i, i+10);
+    }
+    for (i = 0; i < expr.length/10; i++) {
+        for (j = 9, k = 0; j >= 0; j-=2, k++) {
+            a[k] = arrWords[i][j-1] + arrWords[i][j];
+        }
+        a.reverse();
+        for (ii = 0; ii < a.length; ii++) {
+            if (a[ii] ==='**') b[ii] = '*';
+            else if (a[ii] === '00') b[ii] = '';
+            else if (a[ii] === '10') b[ii] = '.';
+            else if (a[ii] === '11') b[ii] = '-';
+        }
+        if (MORSE_TABLE[b.join('')]) strResult = strResult + MORSE_TABLE[b.join('')];
+        else strResult = strResult + ' ';
+       
+    }
+    return strResult; 
+    
 }
 
 module.exports = {
